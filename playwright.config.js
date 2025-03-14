adda37170b630a26482aea28285e60cbf3d73377
Vikas2593 <vikas.genius93@gmail.com>
2025-03-14 15:04:28 +0530

    updated playwirght.conf

--- playwright.config.js
+++ playwright.config.js
@@ -23,10 +23,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['html', { outputFolder: 'test-results/html' }],
-    ['junit', { outputFile: 'test-results/test-results.xml' }]
-  ],
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
